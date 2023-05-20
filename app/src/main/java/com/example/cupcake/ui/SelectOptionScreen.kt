e4bdfd7d729a8c9ed76439263022e09e63494896
Sourabh Kumar <20010001030@gateway.edu.in>
2023-05-21 00:01:35 +0530

    Added modifiers

--- app/src/main/java/com/example/cupcake/ui/SelectOptionScreen.kt
+++ app/src/main/java/com/example/cupcake/ui/SelectOptionScreen.kt
@@ -18,6 +18,7 @@ package com.example.cupcake.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
@@ -33,6 +34,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,69 +52,82 @@ fun SelectOptionScreen(
     subtotal: String,
     options: List<String>,
     onSelectionChanged: (String) -> Unit = {},
-    // TODO: add onCancelButtonClicked
     onCancelButtonClicked: () -> Unit = {},
-    // TODO: add onNextButtonClicked
     onNextButtonClicked: () -> Unit = {},
     modifier: Modifier = Modifier
-) {
+){
     var selectedValue by rememberSaveable { mutableStateOf("") }
 
-    Column(modifier = modifier.padding(16.dp)) {
-        options.forEach { item ->
-            Row(
-                modifier = Modifier.selectable(
-                    selected = selectedValue == item,
-                    onClick = {
-                        selectedValue = item
-                        onSelectionChanged(item)
-                    }
-                ),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                RadioButton(
-                    selected = selectedValue == item,
-                    onClick = {
-                        selectedValue = item
-                        onSelectionChanged(item)
-                    }
-                )
-                Text(item)
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))){
+            options.forEach { item ->
+                Row(
+                    modifier = Modifier.selectable(
+                        selected = selectedValue == item,
+                        onClick = {
+                            selectedValue = item
+                            onSelectionChanged(item)
+                        }
+                    ),
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    RadioButton(
+                        selected = selectedValue == item,
+                        onClick = {
+                            selectedValue = item
+                            onSelectionChanged(item)
+                        }
+                    )
+                    Text(item)
+                }
             }
+            Divider(
+                thickness = dimensionResource(R.dimen.thickness_divider),
+                modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium))
+            )
+            FormattedPriceLabel(
+                subtotal = subtotal,
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .padding(
+                        top = dimensionResource(R.dimen.padding_medium),
+                        bottom = dimensionResource(R.dimen.padding_medium)
+                    )
+            )
         }
-        Divider(thickness = 1.dp, modifier = modifier.padding(bottom = 16.dp))
-        FormattedPriceLabel(
-            subtotal = subtotal,
-            modifier = Modifier
-                .align(Alignment.End)
-                .padding(top = 16.dp, bottom = 16.dp)
-        )
         Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            OutlinedButton(
-                modifier = Modifier.weight(1f),
-                onClick = { onCancelButtonClicked }) {
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_medium))
+                .weight(1f, false),
+            horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+            verticalAlignment = Alignment.Bottom
+        ){
+            OutlinedButton(modifier = Modifier.weight(1f), onClick = onCancelButtonClicked) {
                 Text(stringResource(R.string.cancel))
             }
             Button(
                 modifier = Modifier.weight(1f),
                 // the button is enabled when the user makes a selection
                 enabled = selectedValue.isNotEmpty(),
-                onClick = { onNextButtonClicked }
+                onClick = onNextButtonClicked
             ) {
                 Text(stringResource(R.string.next))
             }
         }
     }
+
 }
 
 @Preview
 @Composable
-fun SelectOptionPreview() {
+fun SelectOptionPreview(){
     SelectOptionScreen(
         subtotal = "299.99",
-        options = listOf("Option 1", "Option 2", "Option 3", "Option 4")
+        options = listOf("Option 1", "Option 2", "Option 3", "Option 4"),
+        modifier = Modifier.fillMaxHeight()
     )
 }
