7e27b4693a8861114a9fac7ae2623677831e1fb2
Sourabh Kumar <20010001030@gateway.edu.in>
2023-06-14 01:29:40 +0530

    updated test depen updated test depenencies

---
+++ app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
@@ -0,0 +1,37 @@
+package com.example.cupcake.test
+
+import android.icu.util.Calendar
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import com.example.cupcake.CupcakeApp
+import com.example.cupcake.CupcakeScreen
+import com.example.cupcake.R
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+/**
+ * Note: To access to an empty activity, the code uses ComponentActivity instead of
+ * MainActivity.
+ */
+@get:Rule
+val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+private lateinit var navController: TestNavHostController
+@Before
+fun setupCupcakeNavHost() {
+    composeTestRule.setContent {
+        navController = TestNavHostController(LocalContext.current).apply {
+            navigatorProvider.addNavigator(ComposeNavigator())
+        }
+        CupcakeApp(navController = navController)
+    }
+}
